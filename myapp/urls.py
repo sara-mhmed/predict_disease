589556346a142a83020f063c8b129776e755d2de
sara-mhmed <saraashehata10@gmail.com>
2025-11-05 06:01:57 +0200

    final updating

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from myapp.views import run_migrations 
+
 urlpatterns = [
     
     path('', views.predict, name='home'),
@@ -8,5 +8,4 @@ urlpatterns = [
     path('api/general-test/submit/', views.submit_general_test),
     path('api/general-test/results/', views.test_results),
     path('api/general-test/results/<int:result_id>/', views.test_result_detail),
-    path('run-migrations/', run_migrations),
 ]
